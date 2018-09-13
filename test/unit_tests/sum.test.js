fa042e48d1f19ae1667d1184f790d6ac6de67421
kevincampbell <campbell.kevin@gmail.com>
2018-09-12 19:01:11 -0700

    chore(test-coverage):CPCWEB-8348 - PDK | Implement Test Coverage Tool

---
+++ test/unit_tests/sum.test.js
@@ -0,0 +1,6 @@
+// example from https://facebook.github.io/jest/docs/en/getting-started.html
+const sum = require('../sum');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
